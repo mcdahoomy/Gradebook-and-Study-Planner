c095be8eba423864082b6af14179221592b491e7
mcdahoomy <dahop47@gmail.com>
2025-10-13 14:40:24 +0300

    Initial

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("application")
+}
+
+group = "com.dahoomy"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+application {
+    mainClass.set("com.dahoomy.gradebook.Main")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/dahoomy/gradebook/GpaScale.java
@@ -0,0 +1,4 @@
+package com.dahoomy.gradebook;
+
+public class GpaScale {
+}
